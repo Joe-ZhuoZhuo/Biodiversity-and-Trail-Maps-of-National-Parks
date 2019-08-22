e3d4d39689d04a5d99b98dc56b4820db2308ed05
Zhuo Zhuo <josephzhuo@Zhuos-MBP.fios-router.home>
2019-08-21 23:29:51 -0400

    put files in the right places

--- Flask app/static/js/logic.js
+++ Flask app/static/js/logic.js
@@ -1,6 +1,5 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+var queryUrl = " ";
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
